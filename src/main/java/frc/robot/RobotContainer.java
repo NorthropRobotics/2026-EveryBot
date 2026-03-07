a1eab81c2624a890f264fde24f32d2eee3f0932b
Northrop Robotics <NorthropRobotics@gmail.com>
2026-03-07 16:22:51 -0500

    update

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import static edu.wpi.first.units.Units.*;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -38,6 +39,7 @@ public class RobotContainer {
 
     public RobotContainer() {
         configureBindings();
+        CameraServer.startAutomaticCapture();
     }
 
     private void configureBindings() {
@@ -75,6 +77,8 @@ public class RobotContainer {
         joystick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
 
         drivetrain.registerTelemetry(logger::telemeterize);
+
+        
     }
 
     public Command getAutonomousCommand() {
